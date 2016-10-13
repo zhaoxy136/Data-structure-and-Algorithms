4650efeeb2602443de8b03e0c5416e1d4bfd6cf5
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-13 19:55:13 -0400

    Create Solution.java

---
+++ Depth-First-Search/Path-Sum/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//Version 0: (Recursion)
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null){
+            return false;
+        }
+        if(root.left == null && root.right == null && root.val == sum){
+            return true;
+        }
+        
+    //Divide     
+        boolean left = hasPathSum(root.left, sum-root.val);
+        boolean right = hasPathSum(root.right, sum-root.val);
+    //Conquer
+        return (left || right);
+    }
+}
+
