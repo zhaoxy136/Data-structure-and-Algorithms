c67971f4f1415b7350b206d97e220f5999b2f11c
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-15 19:24:00 -0400

    Update Solution.java

--- Breadth-First-Search/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
+++ Breadth-First-Search/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
@@ -56,3 +56,33 @@
     }
 }
 
+//Version 1: One Queue with a reverse operation on odd rows of level before return.
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+        if(root == null){
+            return results;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            ArrayList<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode tmp = queue.poll();
+                level.add(tmp.val);
+                if(tmp.left != null){
+                    queue.offer(tmp.left);
+                }
+                if(tmp.right != null){
+                    queue.offer(tmp.right);
+                }
+            }
+            results.add(level);
+        }
+        for(int i = 1; i < results.size(); i += 2){
+            Collections.reverse(results.get(i));
+        }
+        return results;
+    }
+}
