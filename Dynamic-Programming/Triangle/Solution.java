f537eb149d1e06aa480e76c5e90ccef7811edae1
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-17 13:36:28 -0400

    Update Solution.java

--- Dynamic-Programming/Triangle/Solution.java
+++ Dynamic-Programming/Triangle/Solution.java
@@ -35,3 +35,71 @@ public class Solution {
         return result;
     }
 }
+
+//Version 1: Multiple Iteration
+//Bottom Up
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0){
+            return -1;
+        }
+        if(triangle.get(0) == null || triangle.get(0).size() == 0){
+            return -1;
+        }
+        //state: sum[x][y] = minimum path sum from (x,y) to the bottom
+        int n = triangle.size();
+        int[][] sum = new int[n][n];
+        
+        //Initialize
+        for(int i = 0; i < n; i++){
+            sum[n - 1][i] = triangle.get(n - 1).get(i);
+        }
+        
+        //bottom up
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                sum[i][j] = Math.min(sum[i + 1][j], sum[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        //answer
+        return sum[0][0];
+    }
+}
+
+//Version 2: Memorize Search
+public class Solution {
+    private int[][] sum;
+    private List<List<Integer>> triangle;
+    private int n;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0){
+            return -1;
+        }
+        if(triangle.get(0) == null || triangle.get(0).size() == 0){
+            return -1;
+        }
+        this.triangle = triangle;
+        this.sum = new int[n][n];
+        this.n = triangle.size();
+        //Initialize
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sum[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        return helper(0,0);
+    }
+    //find the minimum path sum from (x,y) to bottom
+    private int helper(int x, int y){
+        if(x >= n){
+            return 0;
+        }
+        if(sum[x][y] != Integer.MAX_VALUE){
+            return sum[x][y];
+        }
+        int left = helper(x+1, y);
+        int right = helper(x+1, y+1);
+        sum[x][y] = Math.min(left,right) + triangle.get(x).get(y);
+        return sum[x][y];
+    }
+}
