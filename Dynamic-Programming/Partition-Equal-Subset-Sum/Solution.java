ab4dd526ba444e2b3740d6e421495a510ba2338b
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-30 17:41:24 -0400

    Update Solution.java

--- Dynamic-Programming/Partition-Equal-Subset-Sum/Solution.java
+++ Dynamic-Programming/Partition-Equal-Subset-Sum/Solution.java
@@ -33,3 +33,32 @@ public class Solution {
 }
 
 //Version 1: 
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sumValue = 0;
+        for(int i = 0; i < n; i++){
+            sumValue += nums[i];
+        }
+        if(sumValue % 2 == 1){ // total sum has to be an even number
+            return false;
+        }
+        int halfValue = sumValue / 2;
+        return findSum(halfValue, nums);
+    }
+    
+    private boolean findSum(int target, int[] nums){
+        //state: f[i]: whether the elements can form exactly number target
+        boolean[] f = new boolean[target + 1];
+        //initialize
+        f[0] = true;
+        //function: f[j] = f[j-nums[i]] || f[j];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = target; j >= nums[i]; j--){
+                f[j] = f[j-nums[i]] || f[j];
+            }
+        }
+        //answer
+        return f[target];
+    }
+}
