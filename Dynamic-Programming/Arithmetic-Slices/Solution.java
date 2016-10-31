38fa73d14993a144504914458cee8a3ee4a44194
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-31 16:43:14 -0400

    Update Solution.java

--- Dynamic-Programming/Arithmetic-Slices/Solution.java
+++ Dynamic-Programming/Arithmetic-Slices/Solution.java
@@ -45,4 +45,17 @@ public class Solution {
 }
 
 //Version 1:
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int curr = 0, sum = 0;
+    for (int i=2; i<A.length; i++)
+        if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+            curr += 1;
+            sum += curr;
+        } else {
+            curr = 0;
+        }
+    return sum;
+    }
+}
 
