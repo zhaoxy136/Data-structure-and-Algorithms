37cdd46b67a546df4283e27adb791a24c02a8b18
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-10-31 14:45:17 -0400

    Create Solution.java

---
+++ Dynamic-Programming/Arithmetic-Slices/Solution.java
@@ -0,0 +1,48 @@
+//Version 0: 
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A == null || A.length < 3){
+            return 0;
+        }
+        //preparation
+        int[] subNum = helper(A);
+        //state: f[i]:number of slices within first i elements
+        int[] f = new int[A.length+1];
+        //initialize
+        f[0] = 0;
+        f[1] = 0;
+        f[2] = 0;
+        //function
+        for(int i = 3; i <= A.length; i++){
+            if(A[i-1] - A[i-2] != A[i-2] - A[i-3]){
+                f[i] = f[i-1];
+            }else{
+                f[i] = f[i-1] + subNum[i-1];
+            }
+        }
+        //answer
+        return f[A.length];
+    }
+    
+    public int[] helper(int[] A){
+        //this array to store the number of subarrays which end with A[i]
+        int[] subNum = new int[A.length];
+        int step = A[1] - A[0];
+        int length = 2;
+        for(int i = 2; i < A.length; i++){
+            int temp = A[i] - A[i-1];
+            if(step == temp){
+                length++;
+            }else{
+                length = 2;
+            }
+            //length = (step == temp) ? length++ : 2;
+            step = temp;
+            subNum[i] = length - 2;
+        }
+        return subNum;
+    }
+}
+
+//Version 1:
+
