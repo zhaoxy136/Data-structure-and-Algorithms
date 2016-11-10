4a8748d5153a8347ed1e1ddb34b25640cd33722a
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-10 12:15:18 -0500

    Create Solution.java

---
+++ Linked-List/Reverse-Linked-List/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+//Version 0: Iteraive
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
+
+//Version 1: Recursive
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
