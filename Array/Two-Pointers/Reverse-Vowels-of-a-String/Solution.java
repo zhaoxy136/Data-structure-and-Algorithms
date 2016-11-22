431e928b9af80b11bca73cd6aca653823fc7e16e
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-22 11:55:42 -0500

    Create Solution.java

---
+++ Array/Two-Pointers/Reverse-Vowels-of-a-String/Solution.java
@@ -0,0 +1,65 @@
+//Version 0: original (14 ms)
+public class Solution {
+    public String reverseVowels(String s) {
+        if (s.length() == 0) {
+            return s;
+        }
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+        StringBuilder sb = new StringBuilder(s);
+        int start = 0;
+        int end = sb.length() - 1;
+        while (start < end) {
+            while (!vowels.contains(sb.charAt(start)) && start < end) {
+                start++;
+            }
+            while (!vowels.contains(sb.charAt(end)) && start < end) {
+                end--;
+            }
+            if (start >= end) {
+                break;
+            }
+            char tmp = sb.charAt(start);
+            sb.setCharAt(start, s.charAt(end));
+            sb.setCharAt(end, tmp);
+            start++;
+            end--;
+        }
+        return sb.toString();
+    }
+}
+//Version 1: (5 ms)
+public class Solution {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        String vowels = "aeiouAEIUO";
+        int start = 0;
+        int end = chars.length - 1;
+        while (start < end) {
+            while (start < end && vowels.indexOf(chars[start]) == -1) {
+                start++;
+            }
+            while (start < end && vowels.indexOf(chars[end]) == -1) {
+                end--;
+            }
+            if (start >= end) {
+                break;
+            }
+            char tmp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = tmp;
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
