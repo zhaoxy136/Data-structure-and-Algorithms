8548424982d78e3020d8347db2a38156fe4380e3
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-23 19:59:38 -0500

    Create Solution.java

---
+++ Array/Product-of-Array-Except-Self/Solution.java
@@ -0,0 +1,37 @@
+//Version 1: O(n) time, O(n) extra space
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i-1] * nums[i-1];
+        }
+        int[] right = new int[nums.length];
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+}
+
+//Version 1: O(n) time and O(1) extra space
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * nums[i];
+        }
+        return res;
+    }
+}
