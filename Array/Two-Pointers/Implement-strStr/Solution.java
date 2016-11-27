05df7c98b460d982ec97deb559b73fe327c38b16
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-27 13:35:22 -0500

    Update Solution.java

--- Array/Two-Pointers/Implement-strStr/Solution.java
+++ Array/Two-Pointers/Implement-strStr/Solution.java
@@ -40,3 +40,45 @@ public class Solution {
     }
 }
 
+//Version 2: KMP Algorithm O(m+n)
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        int i = 0;
+        int j = 0;
+        int[] next = getNext(needle);
+        while (i < haystack.length() && j < needle.length()) {
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j == needle.length()) {
+            return i - j;
+        }
+        return -1;
+    }
+    
+    private int[] getNext(String pattern){
+        int[] next = new int[pattern.length()];
+        next[0] = -1;
+        int k = -1;
+        int j = 0;
+        while (j < pattern.length() - 1) {
+            if (k == -1 || pattern.charAt(k) == pattern.charAt(j)) {
+                k++;
+                j++;
+                if (pattern.charAt(j) != pattern.charAt(k)) {
+                    next[j] = k;
+                } else {
+                    next[j] = next[k];
+                }
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+}
