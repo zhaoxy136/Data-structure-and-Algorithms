672b65dfcc14d5c53f6074e83d1938b016cd6150
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-27 01:52:09 -0500

    Update Solution.java

--- Array/Two-Pointers/Implement-strStr/Solution.java
+++ Array/Two-Pointers/Implement-strStr/Solution.java
@@ -1,41 +1,4 @@
-//Version 0: O(n2)
-public class Solution {
-    public int strStr(String haystack, String needle) {
-        if (haystack.length() < needle.length()) {
-            return -1;
-        }
-        if (needle.length() == 0) {
-            return 0;
-        }
-        int res = -1;
-        int start = 0;
-        int end = 0;
-        while (end < haystack.length()) {
-            while (end < haystack.length() && haystack.charAt(end) != needle.charAt(start)) {
-                end++;
-            }
-            res = end;
-            while (start < needle.length() && end < haystack.length()) {
-                if (needle.charAt(start) != haystack.charAt(end)) {
-                    break;
-                }
-                start++;
-                end++;
-            }
-            if (start == needle.length()) {
-                return res;
-            }
-            if (end == haystack.length()) {
-                return -1;
-            }
-            start = 0;
-            end = res + 1;
-        }
-        return -1;
-    }
-}
-
-//Version 1: O(n2)
+//Version 0: O(m*n)
 public class Solution {
     public int strStr(String haystack, String needle) {
         if (needle.length() == 0) return 0;
@@ -55,3 +18,25 @@ public class Solution {
         return -1;
     }
 }
+
+//Version 1: O(m*n) bruteforce 
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int i = 0;
+        int j = 0;
+        while (i < haystack.length() && j < needle.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i = i - (j - 1);
+                j = 0;
+            }
+        }
+        if (j == needle.length()) {
+            return i - j;
+        }
+        return -1;
+    }
+}
+
