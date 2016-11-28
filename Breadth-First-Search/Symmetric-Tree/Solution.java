17be8aafe7f295f56cde507367a962704f4fa8c4
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2016-11-28 00:17:48 -0500

    Create Solution.java

---
+++ Breadth-First-Search/Symmetric-Tree/Solution.java
@@ -0,0 +1,44 @@
+//Version 0: Recursion
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return (helper(left.left, right.right) && helper(left.right, right.left));
+    }
+}
+
+//Version 1: Iteration
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) continue;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+        return true;
+    }
+}
