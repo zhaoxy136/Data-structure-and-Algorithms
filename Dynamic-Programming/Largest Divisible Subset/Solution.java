0b99ba8e290994856cab685834f8c30b541f64eb
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-01-05 20:10:13 -0500

    Update Solution.java

--- Dynamic-Programming/Largest Divisible Subset/Solution.java
+++ Dynamic-Programming/Largest Divisible Subset/Solution.java
@@ -37,3 +37,30 @@ public class Solution {
 //Version 1: 
 //same idea, but more efficient
 //@copyright https://discuss.leetcode.com/topic/49652/classic-dp-solution-similar-to-lis-o-n-2
+public class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        int[] f = new int[nums.length];
+        int[] prev = new int[nums.length];
+        int lastPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            f[i] = 1;
+            prev[i] = -1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && f[j] + 1 > f[i]) {
+                    f[i] = f[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (f[i] > f[lastPos]) lastPos = i;
+        }
+        res.add(nums[lastPos]);
+        while (prev[lastPos] != -1) {
+            lastPos = prev[lastPos];
+            res.add(nums[lastPos]);
+        }
+        return res;
+    }
+}
