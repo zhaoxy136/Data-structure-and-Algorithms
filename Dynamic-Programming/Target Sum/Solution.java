7461f9f56d9bf1bb06fc6bac8a106586689f92c4
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-01-26 21:34:52 -0500

    Update Solution.java

--- Dynamic-Programming/Target Sum/Solution.java
+++ Dynamic-Programming/Target Sum/Solution.java
@@ -9,4 +9,25 @@ public class Solution {
     }
 }
 
-//Version 1: DP
+//Version 1: DP ,very smart!
+//@copyright:https://discuss.leetcode.com/topic/76243/java-15-ms-c-3-ms-o-ns-iterative-dp-solution-using-subset-sum-with-explanation/2
+public class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 == 1) return 0;
+        return helper(nums, (sum+S)/2);
+    }
+    private int helper(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for (int n : nums) {
+            for (int i = target; i >= n; i--) {
+                dp[i] += dp[i-n];
+            }
+        }
+        return dp[target];
+    }
+}
