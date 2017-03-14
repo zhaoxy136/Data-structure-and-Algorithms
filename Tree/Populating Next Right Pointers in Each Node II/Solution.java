d0aeea83faaf8a1fa803bd10cce08ef87eb53b59
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-03-13 23:20:55 -0400

    Create Solution.java

---
+++ Tree/Populating Next Right Pointers in Each Node II/Solution.java
@@ -0,0 +1,43 @@
+//Version 0:
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode nextLeftmost = null;//head of next level
+        TreeLinkNode prev = null;//leading node of next level!
+        TreeLinkNode cur = root;
+        while (cur != null) {
+            //iterate the current level, meaning connect the next level
+            while (cur != null) {
+                if (cur.left != null) {
+                    if (prev == null) {
+                        nextLeftmost = cur.left;
+                    } else {
+                        prev.next = cur.left;
+                    }
+                    prev = cur.left;
+                }
+                if (cur.right != null) {
+                    if (prev == null) {
+                        nextLeftmost = cur.right;
+                    } else {
+                        prev.next = cur.right;
+                    }
+                    //move to next node
+                    prev = cur.right;
+                }
+                cur = cur.next;
+            }
+            //move to next level
+            cur = nextLeftmost;
+            nextLeftmost = null;
+            prev = null;
+        }
+    }
+}
