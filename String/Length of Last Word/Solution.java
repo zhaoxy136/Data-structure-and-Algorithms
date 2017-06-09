a78898768877e84ac4177905f96713768d6f151f
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-06-08 20:03:14 -0400

    Create Solution.java

---
+++ String/Length of Last Word/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+            count++;
+        }
+        return count;
+    }
+}
