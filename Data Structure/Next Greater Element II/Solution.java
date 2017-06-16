8dd8c57d27cfb9a0dea31143cbdab91a649d8e6a
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-06-16 15:07:28 -0400

    Create Solution.java

---
+++ Data Structure/Next Greater Element II/Solution.java
@@ -0,0 +1,18 @@
+//Version 0:
+public class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] res = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length-1; i>= 0; i--) {
+            stack.push(nums[i]);
+        }
+        for (int i = nums.length-1; i>= 0; i--) {
+            while (!stack.isEmpty() && nums[i] >= stack.peek()) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+        return res;
+    }
+}
