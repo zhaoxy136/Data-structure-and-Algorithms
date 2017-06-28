4986ce0c86802c81de4a01be38688fe93292ade4
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-06-27 23:57:14 -0400

    Update Solution.java

--- Dynamic-Programming/Minimum-Path-Sum/Solution.java
+++ Dynamic-Programming/Minimum-Path-Sum/Solution.java
@@ -22,3 +22,24 @@ public class Solution {
         return minSum[m-1][n-1];
     }
 }
+
+//Version 1: O(n) space
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        int[] dp = new int[grid[0].length];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[i] = dp[i-1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[j] = Math.min(dp[j], dp[j-1]) + grid[i][j];
+            }
+        }
+        return dp[grid[0].length-1];
+    }
+}
