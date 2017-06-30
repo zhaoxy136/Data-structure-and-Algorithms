417853a041903f6821cb71255c8225d7fd763792
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-06-30 18:49:39 -0400

    Update Solution.java

--- Linked-List/Add-Two-Numbers/Solution.java
+++ Linked-List/Add-Two-Numbers/Solution.java
@@ -31,3 +31,28 @@ public class Solution {
         return dummy.next;
     }
 }
+
+//
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int tmp = carry;
+            if (l1 != null) {
+                tmp += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                tmp += l2.val;
+                l2 = l2.next;
+            }
+            head.next = new ListNode(tmp % 10);
+            head = head.next;
+            carry = tmp / 10;
+        }
+        if (carry > 0) head.next = new ListNode(carry);
+        return dummy.next;
+    }
+}
