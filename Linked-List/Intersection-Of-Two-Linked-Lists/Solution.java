3a4f20a781306748e0754e5021887012d2a7619c
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-07-08 17:28:49 -0400

    Update Solution.java

--- Linked-List/Intersection-Of-Two-Linked-Lists/Solution.java
+++ Linked-List/Intersection-Of-Two-Linked-Lists/Solution.java
@@ -9,6 +9,35 @@
  *     }
  * }
  */
+//Version 0:
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getLen(headA);
+        int lenB = getLen(headB);
+        ListNode longHead = lenA > lenB ? headA : headB;
+        ListNode shortHead = lenA > lenB ? headB : headA;
+        for (int i = 0; i < Math.abs(lenA - lenB); i++) {
+            longHead = longHead.next;
+        }
+        while (shortHead != null) {
+            if (shortHead == longHead) return shortHead;
+            shortHead = shortHead.next;
+            longHead = longHead.next;
+        }
+        return null;
+    }
+    
+    private int getLen(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+        return len;
+    }
+}
+
+//Version 1:
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null){
