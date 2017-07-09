3249c0a65e0f6e07f610ed2190c7eb4973b9a92a
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-07-08 20:59:13 -0400

    Update Solution.java

--- Linked-List/Merge-Two-Sorted-Lists/Solution.java
+++ Linked-List/Merge-Two-Sorted-Lists/Solution.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//Version 0: iterative
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
@@ -37,3 +38,42 @@ public class Solution {
         return dummy.next;
     }
 }
+
+//Version 1: iterative
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null? Integer.MAX_VALUE : l1.val;
+            int val2 = l2 == null? Integer.MAX_VALUE : l2.val;
+            if (val1 < val2) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+
+//Version 2: Recursive
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+
+
