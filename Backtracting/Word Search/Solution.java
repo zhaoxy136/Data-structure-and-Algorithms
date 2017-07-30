2de24d63512277f9ad18dde96add340ad01864ba
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-07-30 00:31:08 -0400

    Create Solution.java

---
+++ Backtracting/Word Search/Solution.java
@@ -0,0 +1,26 @@
+//Version 0:
+public class Solution {
+    int[] dir = new int[]{1, 0, -1, 0, 1};
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0) && helper(board, word, new boolean[board.length][board[0].length], i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean helper(char[][] board, String word, boolean[][] visited, int row, int col, int start) {
+        if (start >= word.length()) return true;
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length ||
+            visited[row][col] || board[row][col] != word.charAt(start)) return false;
+        visited[row][col] = true;
+        for (int i = 0; i < 4; i++) {
+            if (helper(board, word, visited, row + dir[i], col + dir[i+1], start+1)) return true;
+        }
+        visited[row][col] = false;
+        return false;
+    }
+}
