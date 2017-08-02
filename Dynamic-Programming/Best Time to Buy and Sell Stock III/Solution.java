b7c47266444d0aebe3a6340b80c3fec161e849f1
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-08-02 15:21:06 -0400

    Create Solution.java

---
+++ Dynamic-Programming/Best Time to Buy and Sell Stock III/Solution.java
@@ -0,0 +1,15 @@
+//Version 0:
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, sell1 = 0;
+        int buy2 = Integer.MIN_VALUE, sell2 = 0;
+        
+        for (int price : prices) {
+            sell2 = Math.max(sell2, buy2 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy1 = Math.max(buy1, -price);
+        }
+        return sell2;
+    }
+}
