8320b07f530c318e9f573fcd18d0282d5668603c
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-08-11 14:40:46 -0400

    Create Solution.java

---
+++ Binary-Search/Single Element in a Sorted Array/Solution.java
@@ -0,0 +1,16 @@
+//Version 0:
+public class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid % 2 == 1) mid--;
+            if (nums[mid] == nums[mid+1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
+            }
+        }
+        return nums[start];
+    }
+}
