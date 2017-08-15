5505fba64c493e93a9a2af7aedb1380e38c9f6a7
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-08-15 12:58:17 -0400

    Update Solution.java

--- Design/Insert Delete GetRandom O-1- - Duplicates allowed/Solution.java
+++ Design/Insert Delete GetRandom O-1- - Duplicates allowed/Solution.java
@@ -58,3 +58,49 @@ public class RandomizedCollection {
         return list.get(rand.nextInt(list.size()));
     }
 }
+
+//Version 1:
+public class RandomizedCollection {
+    Map<Integer, Set<Integer>> map;
+    List<Integer> list;
+    Random rand;
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contain = map.containsKey(val);
+        Set<Integer> pos = map.getOrDefault(val, new HashSet<>());
+        pos.add(list.size());
+        map.put(val, pos);
+        list.add(val);
+        return !contain;
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        if (!map.get(val).contains(list.size() - 1)) {
+            int index = map.get(val).iterator().next();
+            int last = list.get(list.size() - 1);
+            map.get(val).remove(index);
+            map.get(val).add(list.size() - 1);
+            map.get(last).remove(list.size() - 1);
+            map.get(last).add(index);
+            list.set(index, last);
+        }
+        map.get(val).remove(list.size() - 1);
+        if (map.get(val).isEmpty()) map.remove(val);
+        list.remove(list.size() - 1);
+        return true;
+    }
+    
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
