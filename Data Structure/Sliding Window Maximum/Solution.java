dd50a7589c10bba0c892552b3f5a29ce6ef93c13
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-08-15 20:12:41 -0400

    Update Solution.java

--- Data Structure/Sliding Window Maximum/Solution.java
+++ Data Structure/Sliding Window Maximum/Solution.java
@@ -1,20 +1,20 @@
 //Version 0:
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] res = new int[nums.length - k + 1];
-        if (nums == null || nums.length == 0 || k == 0) return new int[0];
-        Deque<Integer> deck = new ArrayDeque<>();
+        if (nums == null || nums.length == 0) return new int[0];
+        int[] res = new int[nums.length + 1 - k];
+        Deque<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < nums.length; i++) {
-            while (!deck.isEmpty() && nums[i] > deck.peekLast()) {
-                deck.pollLast();
+            while (!queue.isEmpty() && queue.peekFirst() < nums[i]) {
+                queue.pop();
             }
-            deck.add(nums[i]);
-            if (i + 1 > k && deck.peekFirst() == nums[i-k]) {
-                deck.pollFirst();
+            queue.push(nums[i]);
+            if (i >= k - 1) {
+                res[i+1-k] = queue.peekLast();
+                if (queue.peekLast() == nums[i+1-k])
+                    queue.pollLast();
             }
-            if (i + 1 >= k) {
-                res[i+1-k] = deck.peekFirst();
-            }
+            
         }
         return res;
     }
