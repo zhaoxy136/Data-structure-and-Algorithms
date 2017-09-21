8fa798f55aa76a31600360e5479918c96761aced
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-09-21 19:10:52 -0400

    Update Solution.java

--- Array/Rotate-Array/Solution.java
+++ Array/Rotate-Array/Solution.java
@@ -1,26 +1,48 @@
-//Version 0: three-step-reverse
-public class Solution {
+//Version 0: three-step-reverse //handle negative k
+class Solution {
     public void rotate(int[] nums, int k) {
-        if (nums == null || nums.length == 0){
-            return;
+        if (nums == null || nums.length == 0) return;
+        k = k % nums.length;
+        if (k < 0) {
+            rotate(nums, nums.length + k);
+        } else if (k > 0) {
+            reverse(nums, 0, nums.length - k - 1);
+            reverse(nums, nums.length - k, nums.length - 1);
+            reverse(nums, 0, nums.length - 1);
         }
-        int n = nums.length;
-        k = k % n;
-        reverse(nums, 0, n-k-1);
-        reverse(nums, n-k, n-1);
-        reverse(nums, 0, n-1);
-        
     }
-    private void reverse(int[] nums, int start, int end){
-        if (start > end){
-            return;
-        }
-        int mid = start + (end - start) / 2;
-        for (int i = start; i <= mid; i++){
-            int tmp = nums[i];
-            nums[i] = nums[end+start-i];
-            nums[end+start-i] = tmp;
+    
+    public void reverse(int[] nums, int s, int e) {
+        while (s < e) {
+            int tmp = nums[s];
+            nums[s++] = nums[e];
+            nums[e--] = tmp;
         }
     }
 }
 
+//Version 1: cyclic update
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return;
+        if (k % nums.length == 0) return;
+        if (k < 0) {
+            rotate(nums, nums.length + k);
+        } else {
+            int count = 0;
+            for (int i = 0; i < nums.length && count < nums.length; i++) {
+                int index = i;
+                int prev = nums[i];
+                while ((index + k) % nums.length != i) {
+                    index = (index + k) % nums.length;
+                    int tmp = nums[index];
+                    nums[index] = prev;
+                    count++;
+                    prev = tmp;
+                }
+                nums[i] = prev;
+                count++;
+            }
+        }
+    }
+}
