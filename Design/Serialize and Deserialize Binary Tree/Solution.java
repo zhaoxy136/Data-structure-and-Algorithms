bd7be14abeae026ab6ae64c2cc95da7ab4a98857
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-10-01 14:18:50 -0400

    Update Solution.java

--- Design/Serialize and Deserialize Binary Tree/Solution.java
+++ Design/Serialize and Deserialize Binary Tree/Solution.java
@@ -1,4 +1,59 @@
-//Version 0:
+//Version 0: Level_Order
+//may cause MLE,for extremlt unbalanced tree
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "null";
+        int h = getHeight(root);
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int size = 1;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < size; j++) {
+                TreeNode tmp = queue.poll();
+                if (tmp == null) {
+                    sb.append("null");
+                    queue.add(null);
+                    queue.add(null);
+                } else {
+                    sb.append(tmp.val);
+                    queue.add(tmp.left);
+                    queue.add(tmp.right);
+                }
+                sb.append(",");
+            }
+            size *= 2;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+    
+    public int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left, right) + 1;
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] nodes = data.trim().split(",");
+        return helper(nodes, 0);
+    }
+    public TreeNode helper(String[] nodes, int index) {
+        if (index >= nodes.length) return null;
+        if (nodes[index].equals("null")) {
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[index]));
+        root.left = helper(nodes, index * 2 + 1);
+        root.right = helper(nodes, index * 2 + 2);
+        return root;
+    }
+}
+
+
+//Version 1: PreOrder
 public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
